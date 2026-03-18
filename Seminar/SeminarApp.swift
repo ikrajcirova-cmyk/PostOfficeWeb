053d6622c31f01f5407cfc2c81dda3b29125acf8
ikrajcirova-cmyk <ikrajcirova-cmyk@users.noreply.github.com>
2026-03-18 10:23:21 +0100

    Initial Commit

---
+++ Seminar/SeminarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeminarApp.swift
+//  Seminar
+//
+//  Created by Iveta Krajcirova on 18.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct SeminarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
